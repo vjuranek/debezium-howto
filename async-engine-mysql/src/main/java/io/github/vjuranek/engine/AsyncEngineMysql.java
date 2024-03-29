c6d5e23455047077efaad4d32bc8b8b617373cb5
Vojtech Juranek <vojtech.juranek@gmail.com>
2024-03-29 18:58:55 +0100

    engine: imptove stopping

--- async-engine-mysql/src/main/java/io/github/vjuranek/engine/AsyncEngineMysql.java
+++ async-engine-mysql/src/main/java/io/github/vjuranek/engine/AsyncEngineMysql.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 import io.debezium.engine.ChangeEvent;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.format.Json;
+import io.debezium.engine.format.KeyValueHeaderChangeEventFormat;
 
 /**
  * Debezium embedded engine example from the documentation using MySQL connector.
@@ -21,6 +22,7 @@ public class AsyncEngineMysql {
         props.setProperty("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore");
         props.setProperty("offset.storage.file.filename", "/tmp/offsets.dat");
         props.setProperty("offset.flush.interval.ms", "5000");
+
         // connector properties
         props.setProperty("database.hostname", "localhost");
         props.setProperty("database.port", "3306");
@@ -31,26 +33,26 @@ public class AsyncEngineMysql {
         props.setProperty("schema.history.internal", "io.debezium.storage.file.history.FileSchemaHistory");
         props.setProperty("schema.history.internal.file.filename", "/tmp/schema_history.dat");
 
-        try (DebeziumEngine<ChangeEvent<String, String>> engine = DebeziumEngine
-                .create(Json.class, Json.class, Json.class, "io.debezium.embedded.async.ConvertingAsyncEngineBuilderFactory")
+        DebeziumEngine<ChangeEvent<String, String>> engine = DebeziumEngine
+                .create(KeyValueHeaderChangeEventFormat.of(Json.class, Json.class, Json.class),
+                        "io.debezium.embedded.async.ConvertingAsyncEngineBuilderFactory")
                 .using(props)
                 .notifying(record -> {
-                    System.out.println(record);
-                }).build()
-        ) {
-            ExecutorService executor = Executors.newSingleThreadExecutor();
-            executor.execute(engine);
-            executor.shutdown();
-            try {
-                executor.awaitTermination(1, TimeUnit.HOURS);
-            }
-            catch (InterruptedException e) {
-                System.out.println("Interrupted, shitting down now.");
-                executor.shutdownNow();
-            }
-        }
-        catch (IOException e) {
+                    System.out.println("Key = '" + record.key() + "' value = '" + record.value() + "'");
+                }).build();
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(engine);
+        executor.shutdown();
+        try {
+            TimeUnit.SECONDS.sleep(10);
+            engine.close();
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
+        }
+        finally {
+            executor.shutdownNow();
         }
     }
 }
